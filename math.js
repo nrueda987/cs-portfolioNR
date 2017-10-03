a964f0f6b2b1ec222e75fcd250ead2c6d317872c
Nicholas Rueda <nrueda.sh@mysummitps.org>
2017-10-03 22:18:00 +0000

    vindinium.html

--- math.js
+++ math.js
@@ -7,35 +7,36 @@ var ans1 = document.getElementById("ans1");
 
 //
 button.addEventListener("click", function() {
+// the addEventListener listens for the click of the calculate button
+    //
+    var num1 = +input1.value;
+    var num2 = +input2.value;
+    var operation = drop.value;
+    /* this is a variable and it works by reaching into the html to find "num1" 
+     * and it reaches into the html by "document.getbyid" and it knows which    */
 
-      //
-      var num1 = +input1.value;
-      var num2 = +input2.value;
-      var operation = drop.value;
-
-
-      //
-      if (operation === "+") {
-        ans1.innerHTML = (num1) + (num2);
-      }
-      //
-      if (operation === "-") {
-        ans1.innerHTML = (num1) - (num2);
-      }
-      //
-      if (operation === "*") {
-        ans1.innerHTML = (num1) * (num2);
-      }
-      //
-      if (operation === "/") {
-        ans1.innerHTML = (num1) / (num2);
-      }
-      //
-      if (operation === "^") {
-        ans1.innerHTML =Math.pow(num1, num2)
-      }
-//
- if (operation === "%") {
+    //
+    if (operation === "+") {
+      ans1.innerHTML = (num1) + (num2);
+    }
+    // these are the operation for the calculator which in this case is adding "+"
+    if (operation === "-") {
+      ans1.innerHTML = (num1) - (num2);
+    }
+    //
+    if (operation === "*") {
+      ans1.innerHTML = (num1) * (num2);
+    }
+    //
+    if (operation === "/") {
+      ans1.innerHTML = (num1) / (num2);
+    }
+    //
+    if (operation === "^") {
+      ans1.innerHTML =Math.pow(num1, num2)
+    }
+    //
+    if (operation === "%") {
         ans1.innerHTML = (num1) % (num2);
-}
-    });
+    }
+}); 
